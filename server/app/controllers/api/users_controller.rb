103fb98f50a44f803e34bf7be3c0f9283f842bc9
misheMatcha <mishe.vong@gmail.com>
2020-12-09 13:12:26 -0800

    Fix user params

--- server/app/controllers/api/users_controller.rb
+++ server/app/controllers/api/users_controller.rb
@@ -12,6 +12,6 @@ class Api::UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:name, :username, :email, :password)
   end
 end
