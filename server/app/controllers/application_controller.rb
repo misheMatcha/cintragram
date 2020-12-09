4d32247d18f36ebdc38dc2f3e071271df67a4a17
misheMatcha <mishe.vong@gmail.com>
2020-12-09 12:56:51 -0800

    Add auth private methods in application controller

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -10,4 +10,22 @@ class ApplicationController < ActionController::Base
   def logged_in?(user)
     !!current_user
   end
+
+  def login(user)
+    user.reset_session_token!
+    session[:session_token] = user.session_token
+    @current_user = user
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def require_logged_in
+    unless current_user
+      render json: {base: ["Invalid credentials"]},
+      status: 401
+    end
+  end
 end
