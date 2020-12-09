bff53c599473a6c4a61e6b04b03707084a89e966
misheMatcha <mishe.vong@gmail.com>
2020-12-09 12:48:06 -0800

    Add model validations and find credentials

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :name, :username, :email, :password_digest, :session_token, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
+
+  attr_reader :password
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    user && user.valid_password?(password) ? user : nil
+  end
 end
