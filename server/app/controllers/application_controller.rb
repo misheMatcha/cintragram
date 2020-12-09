e678a9ff5f7ef46dd82d8dac86c82b356780d265
misheMatcha <mishe.vong@gmail.com>
2020-12-09 12:54:47 -0800

    Add controller helper methods

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :logged_in?
+
+  private
+  def current_user
+    return nil unless session[:session_token]
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in?(user)
+    !!current_user
+  end
 end
