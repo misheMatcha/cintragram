f9d136fc40750a94a871227b77abb40dabab0877
misheMatcha <mishe.vong@gmail.com>
2020-12-09 12:49:47 -0800

    Add password setting and checking

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -10,4 +10,13 @@ class User < ApplicationRecord
     user = User.find_by(username: username)
     user && user.valid_password?(password) ? user : nil
   end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
 end
